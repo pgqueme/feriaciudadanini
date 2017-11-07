91c756db02555cf0d216fb812a016274cfd88db4
Pablo Quemé <pgqueme@gmail.com>
2017-11-07 15:44:15 -0600

    Agregando REST endpoints

--- feria-ciudad-anini.php
+++ feria-ciudad-anini.php
@@ -11,8 +11,42 @@
 
     define("ABS_PATH", dirname(__FILE__));
 
+    //Register the route
+    add_action( 'rest_api_init', function () {
+        register_rest_route( 'anini/v1', '/excel_contrato', array(
+            'methods' => 'POST',
+            'callback' => 'generar_excel_contrato',
+        ));
+        register_rest_route( 'anini/v1', '/prueba', array(
+            'methods' => 'GET',
+            'callback' => 'prueba',
+        ));
+    });
+    
+    function prueba(){
+        return "TEST";
+    }
+
+    //Genera archivo Excel de plantilla para un contrato
+    function generar_excel_contrato($data){
+        $id = $data['id'];
+
+        $child_args = array(
+            'post_type' => 'stands',
+            'numberposts' => -1,
+            'meta_key' => 'wpcf-stand',
+            'orderby' => 'meta_value',
+            'order' => 'ASC',
+            'meta_query' => array(array('key' => '_wpcf_belongs_contratos_id', 'value' => $id))
+        );
+        $child_posts = get_posts($child_args);
+
+        return $child_posts;
+    }
+
     //Registrar el shortcode, primero va el nombre y luego el nombre de la función
     add_shortcode('tabla1', 'generar_tabla1');
+
 
     function generar_tabla1($attributes) {
         //Array que va a contener todos los registros   
